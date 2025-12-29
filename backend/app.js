88ebd9f863accdb91e93c0c94d52c380a9799483
Avanish Kumar <146574431+avanishkumar5035@users.noreply.github.com>
2025-12-29 23:00:30 +0530

    29 Dec MERN-Project(ecommerce) second commit

--- backend/app.js
+++ backend/app.js
@@ -7,9 +7,10 @@ app.use(express.json());
 
 // Route import
 const product = require("./routes/productRoute");
-
+const user = require("./routes/userRoute");
 // Route mount
 app.use("/api/v1", product);
+app.use("/api/v1", user);
 
 
 // Middleware for Error 
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,5 @@
 const Product = require("../models/productModel");
 const ErrorHander = require("../utils/errorhander");
-
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
 const ApiFeatures = require("../utils/apifeatures");
 
