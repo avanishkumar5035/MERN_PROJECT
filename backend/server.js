2bea3f5babc8ef8eeab5882971721c76c28b637f
avanishkumar5035 <146574431+avanishkumar5035@users.noreply.github.com>
2026-01-18 23:40:05 +0530

    7th Day commit

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,14 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const connectDatabase = require("./config/database");
 
+// Handling Uncaught Exception 
+
+process.on("uncaughtException",(err)=>{
+    console.log(`Error: ${err.message}`);
+    console.log(`Sutting down the server due to Uncaught Exception`);
+    process.exit(1);
+});
+
 
 // config
 
@@ -16,6 +24,8 @@ const server = app.listen(process.env.PORT,()=>{
     
     console.log(`Server is working on http://localhost:${process.env.PORT}`)
 });
+
+
 
 
 // Unhandled Promise Rejection
